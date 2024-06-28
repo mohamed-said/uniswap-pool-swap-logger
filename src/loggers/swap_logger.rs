51a24629e812efe75b6cb61de45d23a7a3048420
Mohamed Said <mohamed.said.fci@gmail.com>
2024-06-28 22:37:56 +0200

    better documentation

--- src/loggers/swap_logger.rs
+++ src/loggers/swap_logger.rs
@@ -19,6 +19,12 @@ pub struct SwapLogger {
     // Maximum number allowed for organization depth
     // TODO
     //_max_reorg: usize,
+
+    // TODO make the accepted types dynamic
+    // to support multiple swapping pools
+    // not only DAI/USDC
+    // _amount_type_0: AmountType,
+    // _amount_type_1: AmountType,
 }
 
 impl SwapLogger {
@@ -39,14 +45,34 @@ impl SwapLogger {
 
         let res: Vec<web3::types::Log> = self
             .web3_instance
+            // Eth object to get acces to the EPC functions
             .eth()
+            // get all logs matching a given filter object
             .logs(
+                // build the filter
                 web3::types::FilterBuilder::default()
+                    // sets the internal "Filter" type object's
+                    // block_hash value
                     .block_hash(block_hash)
+                    // takes a single address
+                    // for more information about why it takes a vector
+                    // refer to https://docs.rs/ethers/latest/ethers/core/types/enum.ValueOrArray.html
                     .address(vec![contract_address])
+                    // An event topic is a part of the event logging mechanism
+                    // used by smart contracts to record information on the blockchain.
+                    // These logs are stored in transaction receipts
+                    // and can be efficiently filtered and searched.
+                    // Each log entry has -up to- four "topics,"
+                    // which are 32-byte values used to index the log.
+                    //    Topic 0: The hash of the event signature.
+                    //    Topic 1: The from address (indexed).
+                    //    Topic 2: The to address (indexed).
+                    //    Data: The value (not indexed).
                     .topics(Some(vec![swap_event_signature]), None, None, None)
+                    // returns a Filter object with all the specified fields
                     .build(),
             )
+            // call the inner future which fetches the logs
             .await?;
 
         Ok(res)
@@ -54,28 +80,67 @@ impl SwapLogger {
 
     /// subscribes to the latest updates and handles logs printing whenever a new block is ready
     pub async fn display_logs(&self) -> Result<()> {
+        // create a new Event object
+        // the default event name is now swap
         let swap_event = self
             .contract
+            // return ethabi::Contract object, where we can get
+            // access to the down layer ABI related methods
             .abi()
+            // accesses the list of events in tha ABI
+            // and retrieves all events by the given name
             .events_by_name("Swap")?
+            // assuming we have only one event by the given name
+            // we capture that event
+            // returns ethabi::Event object
             .first()
+            // handle error if the given event name is invalid
             .ok_or_else(|| Box::new(LoggerError::FailedToRetrieveEvent))?;
 
+        // retrieve the signature of the event
+        // an event signature is a unique identifier
+        // for a specific event defined within a smart contract.
+        // When an event is emitted (triggered) by a smart contract,
+        // it is recorded in the transaction logs,
+        // which are part of the blockchain's transaction receipts.
+        // These logs can be indexed and queried to listen for specific events.
+        // Which is exactly what we want to do
         let swap_event_signature = swap_event.signature();
 
         // create a heads subscription
         let mut block_stream = self
             .web3_instance
+            // accesss the RPC subscribe methods from the eth namespace
+            // retuerns web3::api::EthSubscribe object
+            // including the available subscription methods
+            // An Ethereum namespace subscription
+            // refers to a method of listening to and receiving real-time updates
+            // for certain events or data changes on the Ethereum blockchain.
+            // This is commonly done through WebSocket connections
+            // or other subscription mechanisms provided by Ethereum nodes or APIs
             .eth_subscribe()
+            // A "new heads" subscription in  Ethereum refers to
+            // subscribing to notifications about new blocks (also called "block headers")
+            // being added to the blockchain.
+            // This allows you to receive real-time updates whenever a new block is mined.
+            //
+            // Returns a subscription stream of blocks (since we subscribe to get blocks)
+            // A subscription stream is a stream of notifications from a subscription.
+            // Given a type deserializable from rpc::Value and a subscription id,
+            // yields items of that type as notifications are delivered.
             .subscribe_new_heads()
             .await?;
 
         while let Some(Ok(block)) = block_stream.next().await {
-            // returns a list of all the "Swap" events in the current block
+            // a list of all the "Swap" events in the current block
             let swap_logs_in_block = self
+                // filter the current block's events for only the swap events
+                // or, in other words, the events with the given signature
                 .filter_swap_events(block.hash.unwrap(), swap_event_signature)
                 .await
+                // anyhow error message
                 .context("Filter swap events failed")?;
+
             let mut block_error = false;
             for log in swap_logs_in_block {
                 // parse each Log's raw data into a more readable format
@@ -108,6 +173,7 @@ impl SwapLogger {
     ///     The receiver address,
     /// }
     fn print_log_formatted(log: web3::ethabi::Log) -> Result<()> {
+        // Amounts to be transferred (given/received)
         let mut amount0: String = String::new();
         let mut amount1: String = String::new();
 
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use loggers::swap_logger::SwapLogger;
 async fn main() -> Result<()> {
     // set the environment variable for your Infura endpoint URL
     if let Ok(websocket_infura_endpoint) = std::env::var("WEBSOCKET_INFURA_ENDPOINT") {
+        // create a web3 object
         let web3 = web3::Web3::new(
             web3::transports::ws::WebSocket::new(&websocket_infura_endpoint).await?,
         );
@@ -19,6 +20,7 @@ async fn main() -> Result<()> {
                 .as_slice(),
         );
 
+        // create a Contract object with the given contract address and Json ABI
         let contract = web3::contract::Contract::from_json(
             web3.eth(),
             contract_address,
@@ -27,6 +29,7 @@ async fn main() -> Result<()> {
 
         // Inject the contract and the web3 object into the logger
         let logger = SwapLogger::new(contract.clone(), web3.clone());
+        // call the async method display logs
         logger.display_logs().await?;
     } else {
         panic!("INFURA endpoint env var is not set!");
